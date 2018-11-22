ccbdc76fd59efe66b00659c973707ac67dc63cba
David Schweizer <d.a.schweizer@gmail.com>
2018-11-22 21:49:30 +0100

    options form (nog niet goed)

--- Tek1/ConfigurationForm.cs
+++ Tek1/ConfigurationForm.cs
@@ -22,9 +22,10 @@ namespace Tek1
 
         public void DoSaveData()
         {
+            Heuristics.SetHeuristicDescriptions(clbHeuristics.Items.OfType<string>().ToList()); // set the index 
             for (int i = 0; i < clbHeuristics.Items.Count; i++)
                 Heuristics.SetHeuristicEnabled(clbHeuristics.Items[i].ToString(), clbHeuristics.GetItemChecked(i));
-        } 
+        }
 
         public void DoSetData(TekHeuristics heuristics)
         {
@@ -35,11 +36,46 @@ namespace Tek1
             List<string> descriptions = Heuristics.GetHeuristicDescriptions();
             foreach (string description in descriptions)
                 clbHeuristics.Items.Add(description, Heuristics.GetHeuristicEnabled(description));
+            EnableUpDownButtons();
         }
 
-        private void bSave_Click(object sender, EventArgs e)
+        private void EnableUpDownButtons()
         {
-            DoSaveData();        
+            int index = clbHeuristics.SelectedIndex;
+            bUp.Enabled = index > 0;
+            bDown.Enabled = index >= 0 && index < clbHeuristics.Items.Count;
+        }
+
+        private void DoMoveSelected(int direction)
+        {
+            int index = clbHeuristics.SelectedIndex;
+            if (index < 0)
+                return;
+            bool isChecked = clbHeuristics.GetItemChecked(index);
+            int newIndex = index + direction;
+            if (newIndex < 0 || newIndex >= clbHeuristics.Items.Count)
+                return;
+            object selected = clbHeuristics.SelectedItem;
+            clbHeuristics.Items.Remove(selected);
+            clbHeuristics.Items.Insert(newIndex, selected);
+            clbHeuristics.SetItemChecked(newIndex, isChecked);
+            clbHeuristics.SetSelected(newIndex, true);
+            EnableUpDownButtons();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DoMoveSelected(-1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DoMoveSelected(1);
+        }
+
+        private void clbHeuristics_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            EnableUpDownButtons();
         }
     }
 }
